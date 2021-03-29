063e9fd84010845f4e9a3339a7e447452341d4af
hayato <shu_mon_win2@yahoo.co.jp>
2021-03-29 10:43:57 +0900

    マイページの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,28 @@
 class UsersController < ApplicationController
 
   def edit
+    @user = User.find(params[:id])
+
+    redirect_to root_path if current_user.id != @user.id
   end
 
   def update
-    current_user.update(user_params)
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      redirect_to edit_user_path(current_user.id)
+    end
+  end
+
+  def show
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:nickname, :email)
+    params.require(:user).permit(:nickname, :email, :password)
   end
+  
 end
+
+

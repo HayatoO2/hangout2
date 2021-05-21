76c7382e973c1a6575dd72f12a106a7b0b6f16be
HayatoO2 <79070012+HayatoO2@users.noreply.github.com>
2021-05-21 13:55:09 +0900

    Update presents_controller.rb

--- app/controllers/presents_controller.rb
+++ app/controllers/presents_controller.rb
@@ -7,10 +7,10 @@ class PresentsController < ApplicationController
 
     if params[:present] == nil 
       @present = Present.new(enter_time: Date.today,  share_id: params[:share_id])
-      @presents = Present.where('enter_time <= ? and leave_time > ?',Date.today,Date.today, share_id: params[:share_id])
+      @presents = Present.where('enter_time <= ? and leave_time > ?',Date.today,Date.today)
     else
       @present = Present.new(enter_time: params[:present][:enter_time],  share_id: params[:share_id], user_id: current_user.id)
-      @presents = Present.where('enter_time <= ? and leave_time >= ?', @present.enter_time,@present.enter_time, share_id: params[:share_id])
+      @presents = Present.where('enter_time <= ? and leave_time >= ?', @present.enter_time,@present.enter_time)
       
     end
       @out_user = Present.in_out(@presents,@present)
